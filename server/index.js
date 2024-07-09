362e1fde1dc40252703e0d650864a6317c74f14b
fast-tortoise <1rn18is121@gmail.com>
2024-07-10 00:54:02 +0530

    adding routes, controllers

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import {fileURLToPath} from 'url';
-//import {register} from "./controllers/auth.js";
+import {register} from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
 
 // CONFIGURATIONS
 
@@ -39,7 +40,11 @@ const storage = multer.diskStorage({
 const upload  = multer({ storage });
 
 // Routes with files
-//app.post("/auth/register", upload.single("picture"), register);
+app.post("/auth/register", upload.single("picture"), register);
+
+//ROUTES
+app.route("/auth", authRoutes);
+
 
 // MONGOOSE SETUP
 
